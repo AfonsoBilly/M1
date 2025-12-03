b158a307c13457c588b0dfa264fe23f840ad1164
Afonso "the strongest" Bilimória <ponsasbilimoria@gmail.com>
2025-12-03 17:23:53 +0000

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -50,3 +50,26 @@ char	*update_stash(char *stash)
 	free(stash);
 	return (new_stash);
 }
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*line;
+	int			bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(stash, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(stash), stash = NULL, NULL);
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin(stash, buffer);
+	}
+	line = extract_line(stash);
+	stash = update_stash(stash);
+	return (line);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,9 +1,20 @@
 #ifndef GET_NEXT_LINE_H
+
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
+# include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 95
+# endif
 
 char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *s);
+int		ft_strlen(char *s);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
 
 #endif
